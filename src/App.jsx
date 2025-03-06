b4ed7ed883aa7ae06228bbc6919e9044e2de3fac
Kubat <Kubatb990@gmail.com>
2025-03-06 17:42:07 +0300

    Save full hw

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+import { Footer } from "./components/Footer";
+export const App = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
---
+++ src/components/UI/Card.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "./Card.css";
+export const Card = () => {
+  return <div>Card</div>;
+};
